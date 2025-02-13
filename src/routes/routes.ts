c55bc4121265708ddd22e92835bbd24864d12b9d
OlaiaAly <olaiaaly@gmail.com>
2025-02-13 23:02:23 +0200

    Enhance API documentation and user model; add user fetching, updating, and deleting routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,6 +6,9 @@ import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
+/**
+ * Interface representing a User.
+ */
 interface User {
   id: string;
   name: string;
@@ -14,12 +17,40 @@ interface User {
   password: string;
 }
 
+/**
+ * Registers routes for the Fastify application.
+ *
+ * @param app - The Fastify application instance.
+ */
 export default function routes(app: FastfifyTypedInstance): void {
-  // Route handler for fetching users
+  /**
+   * Route handler for fetching users.
+   *
+   * GET /users
+   *
+   * Query Parameters:
+   * - name (optional): Filter users by name.
+   * - email (optional): Filter users by email.
+   * - telephone (optional): Filter users by telephone.
+   *
+   * Responses:
+   * - 200: Array of users.
+   * - 500: Error message.
+   */
   app.get("/users", {
     schema: {
-      description: "Fetch users",
       tags: ["users"],
+      querystring: z.object({
+        name: z.string().optional().nullable(),
+        email: z.string().optional().nullable(),
+        telephone: z.string().optional().nullable(),
+      }),
+      description: "Fetch users",
+      docs: {
+        summary: "Retrieves a list of users.",
+        description:
+          "This endpoint fetches all users, optionally filtered by name, email, or telephone.",
+      },
       response: {
         200: z.array(
           z.object({
@@ -29,68 +60,116 @@ export default function routes(app: FastfifyTypedInstance): void {
             telephone: z.string(),
           })
         ),
+        500: z.object({
+          error: z.string(),
+        }),
       },
     },
-    handler: async () => {
+    handler: async (req, reply) => {
       let users: User[] = [];
       try {
-        users = await prisma.user.findMany();
-      } catch (error) {
-        // console.error("Error getting users:", error);
+        if (Object.keys(req.query as object).length > 0) {
+          const { name, email, telephone } = req.query as {
+            name: string;
+            email: string;
+            telephone: string;
+          };
+          users = await prisma.user.findMany({
+            where: {
+              OR: [
+                { name: { contains: name, mode: "insensitive" } }, // Added insensitive mode
+                { email: { contains: email, mode: "insensitive" } }, // Added insensitive mode
+                { telephone: { contains: telephone } },
+              ],
+            },
+          });
+          reply.status(200).send(users);
+        } else {
+          users = await prisma.user.findMany();
+          reply.status(200).send(users);
+        }
+      } catch (error: any) {
+        console.error("Error getting users:", error);
+        reply.status(500).send({ error: "Failed to get users" });
       }
-      return users;
     },
   });
 
-  app.post(
-    "/users",
-    {
-      schema: {
-        description: "Create a new user",
-        tags: ["users"],
-        body: z.object({
-          name: z.string(),
-          telephone: z.string(),
-          email: z.string().email(),
-          password: z.string(),
+  /**
+   * Route handler for fetching a user by ID.
+   *
+   * GET /users/:id
+   *
+   * Path Parameters:
+   * - id: The ID of the user to fetch.
+   *
+   * Responses:
+   * - 200: User data.
+   * - 404: User not found.
+   * - 500: Error message.
+   */
+  app.get("/users/:id", {
+    schema: {
+      tags: ["users"],
+      description: "Fetch user by id",
+      docs: {
+        summary: "Retrieves a list of users.",
+        description: "This endpoint fetches a single one users by its ID.",
+      },
+      response: {
+        200: z.array(
+          z.object({
+            id: z.string(),
+            name: z.string(),
+            email: z.string(),
+            telephone: z.string(),
+          })
+        ),
+        500: z.object({
+          error: z.string(),
         }),
-        response: {
-          201: z
-            .object({
-              id: z.string(),
-              name: z.string(),
-              telephone: z.string(),
-              email: z.string().email(),
-            })
-            .describe("User created successfully"),
-        },
       },
     },
-    // This route handler creates a new user
-    async (request, reply) => {
-      const { name, telephone, email, password } = request.body;
-      // let id: string = randomUUID();
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
+    handler: async (req, reply) => {
+      const { id } = req.params as unknown as { id: string };
       try {
-        const newUser = await prisma.user.create({
-          data: {
-            name: name,
-            email: email,
-            telephone: telephone,
-            password: hashedPassword,
-          },
-        }); // Exclude password from the response
+        const user = await prisma.user.findUnique({
+          where: { id },
+        });
 
-        const { password, ...userWithoutPassword } = newUser;
-        return userWithoutPassword;
+        console.log("User:", user);
+
+        if (user) {
+          const { password: _, ...rest } = user; // Destructure and exclude password
+          reply.status(200).send([rest]);
+        } else {
+          reply.status(404).send({ error: "User not found" });
+        }
       } catch (error) {
-        console.error("Error creating user:", error);
+        console.error("Error getting user:", error);
+        reply.status(500).send({ error: "Failed to get user" });
       }
-    }
-  );
+    },
+  });
 
+  /**
+   * Route handler for updating a user by ID.
+   *
+   * PUT /users/:id
+   *
+   * Path Parameters:
+   * - id: The ID of the user to update.
+   *
+   * Request Body:
+   * - name: The new name of the user.
+   * - password: The new password of the user.
+   * - email: The new email of the user.
+   * - telephone: The new telephone number of the user.
+   *
+   * Responses:
+   * - 200: Updated user data.
+   * - 500: Error message.
+   */
   app.put(
     "/users/:id",
     {
@@ -100,21 +179,30 @@ export default function routes(app: FastfifyTypedInstance): void {
           name: z.string(),
           password: z.string(),
           email: z.string().email(),
+          telephone: z.string().max(9),
         }),
         description: "Updates the user data",
       },
     },
     async (req, reply) => {
       const { id } = req.params as unknown as { id: string };
+
+      const { name, email, telephone } = req.body as unknown as {
+        name: string;
+        email: string;
+        telephone: string;
+      };
       try {
-        const user = await prisma.user.findUnique({
+        const updatedUser = await prisma.user.update({
           where: { id },
+          data: { name, email, telephone },
         });
-        if (user) {
-          const { password, ...rest } = user; // Destructure and exclude password
+
+        if (updatedUser) {
+          const { password, ...rest } = updatedUser; // Destructure and exclude password
           return rest; // Return the remaining properties
         } else {
-          return user;
+          return updatedUser;
         }
       } catch (error) {
         console.error("Error getting user:", error);
@@ -122,6 +210,19 @@ export default function routes(app: FastfifyTypedInstance): void {
     }
   );
 
+  /**
+   * Route handler for deleting a user by ID.
+   *
+   * DELETE /users/:id
+   *
+   * Path Parameters:
+   * - id: The ID of the user to delete.
+   *
+   * Responses:
+   * - 204: No content.
+   * - 404: User not found.
+   * - 500: Error message.
+   */
   app.delete(
     "/users/:id",
     {
--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { fastifySwagger } from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import routes from "./routes/routes";
 
-const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+const app = fastify({ logger: false }).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -18,7 +18,14 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(fastifyCors, { origin: "*" });
 
 app.register(fastifySwagger, {
-  openapi: { info: { title: "Typed API", version: "0.1.0" } },
+  // exposeHeadRoutes: true,
+  openapi: {
+    info: {
+      title: "Typed API",
+      version: "0.1.0",
+      description: "My awesome API",
+    }, // Add a general description here
+  },
   transform: jsonSchemaTransform,
 });
 
@@ -26,6 +33,10 @@ app.register(fastifySwaggerUi, { routePrefix: "/docs" });
 
 app.register(routes);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log(`Server running on port 3333 at http://localhost:3333`);
+// app.listen({ port: 3333 }).then(() => {
+//   console.log(`Server running on port 3333 at http://localhost:3333`);
+// });
+
+app.listen({ port: 3333 }, () => {
+  console.log("Server listening on port 3000");
 });
