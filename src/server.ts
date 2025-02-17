0811dfc1fca23afe0e1a02f25ddcd86a115251a3
OlaiaAly <olaiaaly@gmail.com>
2025-02-17 16:54:11 +0200

    Update project configuration to include fastify-static, adjust TypeScript settings, and enhance server routing

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,16 @@ import {
 import { fastifySwagger } from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import routes from "./routes/routes";
+import path from "path";
+import { fileURLToPath } from "url";
+import fastifyStatic from "@fastify/static";
 
 const app = fastify({ logger: false }).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+const PORT = parseInt(process.env.APP_PORT || "3000", 10); // Use 3000 as a fallback
 
 app.register(fastifyCors, { origin: "*" });
 
@@ -29,14 +34,32 @@ app.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 });
 
+/**
+ * @description Serves the main application page (index.html).
+ * @route GET /
+ * @returns {string} 200 - The main application page.
+ * @returns {string} 500 - Error loading page.
+ * @tags Static
+ * @example response - 200 - The main application page.
+ **/
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "./"),
+  prefix: "/",
+});
+
+app.get("/", async (req, res) => {
+  try {
+    await res.sendFile("./src/index.html");
+  } catch (err) {
+    console.error("Error sending index.html:", err);
+    res.status(500).send("Error loading page"); // Consider a more user-friendly error response
+  }
+});
+
 app.register(fastifySwaggerUi, { routePrefix: "/docs" });
 
 app.register(routes);
 
-// app.listen({ port: 3333 }).then(() => {
-//   console.log(`Server running on port 3333 at http://localhost:3333`);
-// });
-
-app.listen({ port: 3333 }, () => {
-  console.log("Server listening on port 3000");
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
